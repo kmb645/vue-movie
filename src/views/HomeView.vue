1534820c9ee93bc7ab82ce08526346673fb66df0
Masum <mmmasumbillah@gmail.com>
2022-03-20 10:55:08 +0600

    added: markup for search movie, tv show and person

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <form @submit.prevent="" class="search-box">
       <input
         type="text"
-        placeholder="What are you looking for?"
+        placeholder="Search for a movie, tv show, person....."
         v-model="search"
       />
       <input type="submit" value="Search" />
@@ -12,3 +12,49 @@
 </template>
 
 <script></script>
+<style lang="scss" scoped>
+.search-box {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 16px;
+  input {
+    display: block;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+    border-radius: 4px;
+    &[type="text"] {
+      width: 100%;
+      color: #fff;
+      background-color: #496583;
+      font-size: 20px;
+      padding: 10px 16px;
+      margin-bottom: 15px;
+      transition: 0.4s;
+      &::placeholder {
+        color: #f3f3f3;
+      }
+      &:focus {
+        box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+      }
+    }
+    &[type="submit"] {
+      width: 100%;
+      max-width: 300px;
+      background-color: #428883;
+      padding: 16px;
+      border-radius: 8px;
+      color: #fff;
+      font-size: 20px;
+      text-transform: uppercase;
+      transition: 0.4s;
+      &:active {
+        background-color: #388070;
+      }
+    }
+  }
+}
+</style>
